f2a0f032c6e01f5bf969d2d434ca941b1675fb51
nnovic <nnovic>
2009-05-06 16:44:31 +0000

    make a good example of Python binding script.
    git-svn-id: svn://svn.code.sf.net/p/modbuspal/code/ModbusPal@38 42ed8c19-e739-47c8-872a-484d4b446698

--- Examples/CustomBinding.py
+++ Examples/CustomBinding.py
@@ -1,9 +1,37 @@
 from modbuspal.script import PythonBinding
+from java.util import Calendar
 
 class CustomBinding(PythonBinding):
 
   def getSize(self):
-    return 16;
+    return 3*16;
 
+  # Assuming that the provided "value" is a Unix timestamp (32-bit integer
+  # value representing the number of seconds since 1st January 1970),
+  # this binding will transfom that timestamp to the following formatting:
+  # - register #0 will contain minutes in the high byte, and seconds in the low byte
+  # - register #1 will contain days in the high byte, and hours in the low byte
+  # - register #2 will contain years in the high byte, and months in the low byte
   def getRegister(self,rank,value):
-    return 555;
+    
+    unix_timestamp = long(value);
+    cal = Calendar.getInstance();
+    cal.setTimeInMillis(  unix_timestamp * 1000 );
+    
+    if rank==0 :
+      second = cal.get( Calendar.SECOND );
+      minute = cal.get( Calendar.MINUTE );
+      return minute * 256 + second;
+      
+    elif rank==1 :
+      hour = cal.get( Calendar.HOUR_OF_DAY );
+      day = cal.get( Calendar.DAY_OF_MONTH ) ;
+      return day * 256 + hour;
+      
+    elif rank==2 :
+      month = cal.get( Calendar.MONTH ) ;
+      year = cal.get( Calendar.YEAR) % 100;
+      return year * 256 + month;
+      
+    else:
+      return 0;
